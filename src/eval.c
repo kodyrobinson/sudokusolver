5ed4fe1d5c07c094b6b31a2531d324c5d197d9df
Kody Robinson <kodyrobinson@kodys-air.lan>
2020-05-06 16:22:25 -0400

    Created the dup function.

---
+++ src/eval.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "eval.h"
+
+#define BOARDSIZE 9
+
+static bool dup(val *firstval);
+
+bool boxeval(val *firstval) {
+    return false;
+}
+
+bool roweval(val *firstval) {
+    return !dup(firstval);
+}
+
+bool coleval(val *firstval) {
+    return false;
+}
+
+// Static helper function returns true if the parameter array contains any duplicate values other than 0
+static bool dup(val *firstval) {
+    val temparr[BOARDSIZE] = { 0 };
+    for (int i = 0; i < BOARDSIZE; i++) {
+        temparr[i] = firstval[i];
+    }
+    // Bubble sort the temp array.
+    for (int i = 0; i < BOARDSIZE-1; i++) {
+        for (int n = i; n < BOARDSIZE-1; n++) {
+            if (temparr[n] > temparr[n+1]) {
+                int temp = temparr[n];
+                temparr[n] = temparr[n+1];
+                temparr[n+1] = temp;
+            }
+        }
+    }
+    // Perform a linear search to detect duplicates.
+    for (int i = 0; i < BOARDSIZE-1; i++) {
+        if (temparr[i] == temparr[i+1] && temparr[i] != 0) {
+            return true;
+        }
+    }
+    return false;
+}
