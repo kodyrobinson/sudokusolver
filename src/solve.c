fccbd9d28ae46b848d13042031c4a60bcce4c2f9
Kody Robinson <kodyr@live.unc.edu>
2020-05-07 21:26:32 -0400

    Beginning to implement the function to record potential values for unknown spaces.

--- src/solve.c
+++ src/solve.c
@@ -10,7 +10,13 @@
 #define FILESIZE 162
 #define MASK 0b00001111
 
+typedef struct point {
+    int x;
+    int y;
+} point;
+
 static elem board[BOARDSIZE][BOARDSIZE];
+int unknownleft = BOARDSIZE * BOARDSIZE;
 
 void printboard();
 void initboard(char *boardfilename);
@@ -54,6 +60,7 @@ void initboard(char *boardfilename) {
             bool potentials[10] = { true };
             strcpy((char *)board[i % BOARDSIZE][(i - (i % BOARDSIZE)) / BOARDSIZE].d.potvals, (const char *)potentials);
         } else {
+            unknownleft--;
             board[i % BOARDSIZE][(i - (i % BOARDSIZE)) / BOARDSIZE].known = true;
             board[i % BOARDSIZE][(i - (i % BOARDSIZE)) / BOARDSIZE].d.value = procstring[i];
         }
@@ -61,3 +68,29 @@ void initboard(char *boardfilename) {
     free(boardstring);
     fclose(fp);
 }
+
+void findpotvals(int x, int y) {
+    if (board[x][y].known) {
+        return;
+    }
+    point box[BOARDSIZE];
+    point row[BOARDSIZE];
+    point col[BOARDSIZE];
+    for (int i = 0; i < BOARDSIZE; i++) {
+        point boxpt = {(i % 3) + x - (x % 3), ((i - (i % 3)) / 3) + y - (y % 3)};
+        box[i] = boxpt;
+        point rowpt = {i, y};
+        row[i] = rowpt;
+        point colpt = {x, i};
+        col[i] = colpt;
+    }
+}
+
+
+
+
+
+
+
+
+
